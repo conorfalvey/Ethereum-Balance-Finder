c03a617be28995ff577f9622fcc4f3721e5d660b
Conor Falvey <conor.falvey22@gmail.com>
2019-02-23 10:53:42 -0800

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', function (req, res) {
 })
 
 app.listen(3000, function () {
-  console.log('BitBroker Code Sample listening on port 3000')
+  console.log('Ethereum Balance Finder listening on port 3000')
 })
 
 app.post('/', function (req, res) {
